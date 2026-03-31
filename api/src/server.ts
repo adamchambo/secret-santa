74f5a6ba76f6265147dcda8acad9e62f8c906a8c
Adam C <adam.chamberlain910@gmail.com>
2026-03-31 18:29:42 +1100

    feat(api): implement server entry point and middleware

---
+++ api/src/server.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'; 
+import express, { json, urlencoded } from "express";
+import cors from "cors"; 
+import { secure } from "./middleware/secure.js";
+import { logger } from "./middleware/logger.js";
+import { notfoundHandler } from "./middleware/not-found.js";
+import { errorHandler } from "./middleware/error.js";
+import { appRouter } from "./routes/routes.js";
+
+const app = express(); 
+
+app.use(secure); 
+app.use(logger); 
+app.use(cors()); 
+app.use(json());
+app.use(urlencoded({ extended: true }))
+
+app.use('/api', appRouter); 
+app.use(notfoundHandler);
+app.use(errorHandler); 
+
+const URL = process.env.API_BASE_URL || "http://localhost";
+const PORT = process.env.PORT || 5000; 
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port: ${URL}:${PORT}`)
+});
